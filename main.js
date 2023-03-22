b0334eb79ff03c41684e653825ed5f367acccb1a
UPWAN <96981533+UPWAN2011@users.noreply.github.com>
2023-03-22 18:45:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    } );
+}
+
+console.log('ml5.version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZlBpLWGcs/model.json',modelLoaded);
+function modelLoaded(){
+  console.log('Model Loaded!');
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("name_of_the_object").innerHTML=results[0].label;
+        document.getElementById("accuracy_of_the_object").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
+
